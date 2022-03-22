abdbdb488b6f55c687c28748fafba03f1e57dddc
rahmanShuhin <suhin4000@gmail.com>
2022-03-22 21:49:39 +0600

    updated

--- components/Landing/Landing.js
+++ components/Landing/Landing.js
@@ -12,47 +12,39 @@ const Landing = () => {
       preserveAspectRatio: "xMidYMid slice",
     },
   };
-useEffect((e)=>{
-  const docStyle = document.documentElement.style
-const aElem = document.getElementById('new-style')
-const boundingClientRect = aElem.getBoundingClientRect()
+  useEffect((e) => {
+    const docStyle = document.documentElement.style;
+    const aElem = document.getElementById("new-style");
+    const boundingClientRect = aElem.getBoundingClientRect();
 
-aElem.onmousemove = function(e) {
+    aElem.onmousemove = function (e) {
+      const x = e.clientX - boundingClientRect.left;
+      const y = e.clientY - boundingClientRect.top;
 
-	const x = e.clientX - boundingClientRect.left
-	const y = e.clientY - boundingClientRect.top
-	
-	const xc = boundingClientRect.width/2
-	const yc = boundingClientRect.height/2
-	
-	const dx = x - xc
-	const dy = y - yc
-	
-	docStyle.setProperty('--rx', `${ dy/-1 }deg`)
-	docStyle.setProperty('--ry', `${ dx/10 }deg`)
-	
-}
+      const xc = boundingClientRect.width / 2;
+      const yc = boundingClientRect.height / 2;
 
-aElem.onmouseleave = function(e) {
-	
-	docStyle.setProperty('--ty', '0')
-	docStyle.setProperty('--rx', '0')
-	docStyle.setProperty('--ry', '0')
-	
-}
+      const dx = x - xc;
+      const dy = y - yc;
 
-aElem.onmousedown = function(e) {
-	
-	docStyle.setProperty('--tz', '-25px')
-	
-}
+      docStyle.setProperty("--rx", `${dy / -1}deg`);
+      docStyle.setProperty("--ry", `${dx / 10}deg`);
+    };
 
-document.body.onmouseup = function(e) {
-	
-	docStyle.setProperty('--tz', '-12px')
-	
-}
-},[])
+    aElem.onmouseleave = function (e) {
+      docStyle.setProperty("--ty", "0");
+      docStyle.setProperty("--rx", "0");
+      docStyle.setProperty("--ry", "0");
+    };
+
+    aElem.onmousedown = function (e) {
+      docStyle.setProperty("--tz", "-25px");
+    };
+
+    document.body.onmouseup = function (e) {
+      docStyle.setProperty("--tz", "-12px");
+    };
+  }, []);
 
   return (
     <div className="hero">
@@ -62,30 +54,34 @@ document.body.onmouseup = function(e) {
           <h2>
             Grow your business with{" "}
             <span
-            className='animate-charcter'
+              className="animate-charcter"
               style={{
                 color: "#fff",
                 padding: "10px",
                 fontWeight: "300",
-                fontSize:"148px"
+                fontSize: "148px",
               }}
             >
               SKYLINK CREATIVE
             </span>{" "}
           </h2>
-          <p style={{marginLeft:"12px"}}>
+          <p style={{ marginLeft: "12px" }}>
             It's time to take your business global. Marketing is not one size
             fits all and the world of digital marketing has evolved drastically
             in recent years, but it seems like every small-to-medium sized
             company is doing their own thing. That's where we come in.
           </p>
           <button>
-            <a id="new-style" href="#" data-title="Hire Us"></a>
+            <a
+              id="new-style"
+              href="https://www.facebook.com/skylinkcreative"
+              data-title="Hire Us"
+            ></a>
             {/* <a href="https://www.facebook.com/skylinkcreative">Hire Us</a> */}
-            </button>
+          </button>
         </div>
         <div className="lottie__file">
-          <Lottie options={defaultOptions} height={600} width={600} />
+          <Lottie options={defaultOptions} height={500} width={500} />
         </div>
       </div>
 
